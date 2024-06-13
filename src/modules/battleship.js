cfede94189673d84f7136f4ea9eb12978d2bb39c
JHoldsworth23 <jacobph101@icloud.com>
2024-06-13 20:24:15 +0100

    create a battleship class and export to test

---
+++ src/modules/battleship.js
@@ -0,0 +1,20 @@
+class Battleship {
+    constructor(length, name='unnamed', axis='xAxis') {
+        this.length = length;
+        this.name = name;
+        this.hitCount = 0;
+        this.sunk = false;
+        this.axis = axis;
+    }
+
+    hit() {
+        this.hitCount++;
+        if (this.length === this.hitCount) this.sunk = true;
+    }
+
+    isSunk() {
+        return this.sunk;
+    }
+}
+
+module.exports = Battleship;
