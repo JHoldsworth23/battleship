5df3bbcb14374f3a1f14a9f254f4d21d23f8e8a1
JHoldsworth23 <jacobph101@icloud.com>
2024-06-19 21:25:37 +0100

    add two methods to handle both grid events when a user or enemy selects to attack the grid cell

--- src/modules/gamemanager.js
+++ src/modules/gamemanager.js
@@ -6,10 +6,52 @@ class GameManager {
         this.player = new Player(userName);
         this.enemy = new Player(enemyName, 'computer');
         this.UI = new UserInterface(this.player, this.enemy);
+        this.currentPlayer = this.player;
+        this.isGameOver = false;
     }
     
     initialiseGame() {
         this.UI.renderGrids();
+
+        if (this.enemy.type === 'computer') {
+            this.UI.createPlayerGridEventHandler((e) => e.preventDefault());
+        } else {
+            this.UI.createPlayerGridEventHandler(this.playerGridEventHandler.bind(this));
+        }
+
+        this.UI.createEnemyGridEventHandler(this.enemyGridEventHandler.bind(this));
+
+    }
+
+    playerGridEventHandler(e) {
+        if (!this.isGameOver && this.currentPlayer === this.enemy) {
+            const index = this.UI.playerGridCells.indexOf(e.target);
+            const x = Math.floor(index / 10);
+            const y = index % 10;
+
+            this.player.gameboard.attackCoordinate(x, y);
+            // update UI gameboard grid
+
+            // check if the enemy wins
+
+            this.currentPlayer = this.player;
+        }
+    }
+
+    enemyGridEventHandler(e) {
+        if (!this.isGameOver && this.currentPlayer === this.player) {
+            const index = this.UI.enemyGridCells.indexOf(e.target);
+            const x = Math.floor(index / 10);
+            const y = index % 10;
+
+            if (this.enemy.gameboard.attackCoordinate(x, y)) {
+                // update UI gameboard grid
+                
+                // check if the player wins 
+
+                this.currentPlayer = this.enemy;
+            }
+        }
     }
 
 }
