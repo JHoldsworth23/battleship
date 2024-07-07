f2c618d048b26d79917ea2cdec49465310cc2833
JHoldsworth23 <jacobph101@icloud.com>
2024-07-07 20:35:03 +0100

    create a new js file for enemy AI and create AI class

---
+++ src/modules/ai.js
@@ -0,0 +1,13 @@
+class AI {
+    constructor(enemy, ui, player) {
+        this.enemy = enemy;
+        this.ui = ui;
+        this.player = player;
+        this.nextAttacks = [];
+        this.duringMove = false;
+    }
+
+    makeMove() {
+        
+    }
+}
