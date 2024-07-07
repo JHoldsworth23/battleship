fbde51e6a23d81740922f53d2458892f97bb7f71
JHoldsworth23 <jacobph101@icloud.com>
2024-07-07 20:43:54 +0100

    add two methods to check if the ship has been hit and making a random attack on the player grid

--- src/modules/ai.js
+++ src/modules/ai.js
@@ -8,6 +8,27 @@ class AI {
     }
 
     makeMove() {
-        
+        let x;
+        let y;
+
+        if (this.nextAttacks.length > 0) {
+            // work out the next area when a player ship has been hit
+        } else {
+            this.randomAttack();
+        }
+
+        const hitSuccessful = this.player.gameboard.attackCoordinate(x, y);
+        if (hitSuccessful === 'hit') {
+            this.nextAttacks.push([x, y]);
+        }
+    }
+
+    randomAttack() {
+        let x;
+        let y;
+        do {
+            x = Math.floor(Math.random() * 10);
+            y = Math.floor(Math.random() * 10);
+        } while (this.player.gameboard.board[x][y] === 'hit' || this.player.gameboard.board[x][y] === 'miss');
     }
 }
