522296232dcf7de4167c7a6df1f262a632d3c516
JHoldsworth23 <jacobph101@icloud.com>
2024-07-07 21:01:30 +0100

    create a smart move for enemy to work out the ship location if it has been hit and add the method to check if next attack is valid

--- src/modules/ai.js
+++ src/modules/ai.js
@@ -12,7 +12,25 @@ class AI {
         let y;
 
         if (this.nextAttacks.length > 0) {
-            // work out the next area when a player ship has been hit
+            const [lastHitX, lastHitY] = this.nextAttacks[0];
+            const coordinates = [
+                [lastHitX - 1, lastHitY], [lastHitX + 1, lastHitY],
+                [lastHitX, lastHitY - 1], [lastHitX, lastHitY + 1]
+            ];
+
+            for (let [nextX, nextY] of coordinates) {
+                if (this.validAttack(nextX, nextY)) {
+                    x = nextX;
+                    y = nextY;
+                    break;
+                }
+            }
+
+            if (x === undefined || y === undefined) {
+                this.nextAttacks.shift();
+                this.makeMove();
+                return;
+            }
         } else {
             this.randomAttack();
         }
@@ -30,5 +48,9 @@ class AI {
             x = Math.floor(Math.random() * 10);
             y = Math.floor(Math.random() * 10);
         } while (this.player.gameboard.board[x][y] === 'hit' || this.player.gameboard.board[x][y] === 'miss');
+    }
+
+    validAttack(x, y) {
+        return x >= 0 && x < 10 && y >= 0 && y < 10 && this.player.gameboard.board[x][y] !== 'hit' && this.player.gameboard.board[x][y] !== 'miss';
     }
 }
