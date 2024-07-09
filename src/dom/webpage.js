16565f8d058fc8a2ff16d126625bf0f926b2201b
JHoldsworth23 <jacobph101@icloud.com>
2024-07-09 19:03:08 +0100

    create webpage js file and call the function to create the footer section

---
+++ src/dom/webpage.js
@@ -0,0 +1,13 @@
+const createFooterSection = () => {
+    const footer = document.querySelector('footer');
+    const date = new Date();
+
+    footer.innerHTML = `
+        <p>Copyright ©️ ${date.getFullYear()} JHoldsworth23</p>
+        <a href="https://github.com/JHoldsworth23" target="_blank" rel="noopener noreferrer">
+            <i class="fa-brands fa-github"></i>
+        </a>
+    `;
+}
+
+export default createFooterSection;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import GameManager from "./dom/gamemanager";
+import createFooterSection from "./dom/webpage";
 import './style.css';
+
+createFooterSection();
 
 const gameManager = new GameManager();
 gameManager.initialiseGame();
